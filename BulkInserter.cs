4181b4012e7deaf4f83320e538ed6d6476c42c5a
Nikolaj04 <felsdd04@gmail.com>
2026-03-20 11:40:01 +0100

    1

---
+++ BulkInserter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBopgave
+{
+    public class BulkInserter : IInserter
+    {
+        public void InsertTitles(List<Title_Model> titles, SqlConnection sqlConn)
+        {
+            
+        }
+    }
+}
---
+++ IInserter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBopgave
+{
+    public interface IInserter
+    {
+        void InsertTitles(List<Title_Model> titles, SqlConnection sqlConn);
+    }
+}
