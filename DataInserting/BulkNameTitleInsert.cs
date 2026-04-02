3e7bd8eab79088d4f386752d3471df91de7099b2
Nikolaj04 <felsdd04@gmail.com>
2026-04-02 11:46:11 +0200

    bulknametitle

---
+++ DataInserting/BulkNameTitleInsert.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBopgave.DataInserting
+{
+    public static class BulkNameTitleInsert
+    {
+        public static void NameTitleInserting()
+        {
+            SqlConnection sqlConn = new SqlConnection(
+            "Server=localhost;Database=MovieDB;Integrated security=True;" +
+            "Trusted_Connection=True;TrustServerCertificate=True;");
+            sqlConn.Open();
+
+
+        }
+    }
+}
