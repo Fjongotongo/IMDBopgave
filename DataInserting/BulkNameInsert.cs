302c99d0475aa218c26e6623917ee2dea94b2638
Nikolaj04 <felsdd04@gmail.com>
2026-04-02 10:13:45 +0200

    nameinserter

---
+++ DataInserting/BulkNameInsert.cs
@@ -0,0 +1,55 @@
+﻿using IMDBopgave;
+using IMDBopgave.Inserters;
+using IMDBopgave.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBopgave.DataInserting
+{
+    public static class BulkNameInsert
+    {
+        public static void BulkInsertingName()
+        {
+            List<Name_Model> names = new List<Name_Model>();
+
+            BulkInserterName inserter = new BulkInserterName();
+
+            SqlConnection sqlConn = new SqlConnection(
+            "Server=localhost;Database=MovieDB;Integrated security=True;" +
+            "Trusted_Connection=True;TrustServerCertificate=True;");
+            sqlConn.Open();
+
+            var allLines = File.ReadLines("");
+
+            foreach (string name in allLines.Skip(1))
+            {
+                string[] parts = name.Split('\t');
+                if (parts.Length == 9)
+                {
+                    names.Add(new Name_Model(parts));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid line: " + name);
+                }
+
+                if (names.Count >= 1000000)
+                {
+                    inserter.InsertNames(names, sqlConn);
+                    names.Clear();
+                }
+            }
+
+            if (names.Count > 0)
+            {
+                inserter.InsertNames(names, sqlConn);
+            }
+
+            sqlConn.Close();
+        }
+    }
+}
---
+++ Inserters/BulkInserterName.cs
@@ -0,0 +1,34 @@
+﻿using IMDBopgave.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBopgave.Inserters
+{
+    public class BulkInserterName
+    {
+        public void InsertNames(List<Name_Model> names, SqlConnection sqlConn)
+        {
+            DataTable nameTable = new DataTable();
+            nameTable.Columns.Add("NConst", typeof(string));
+            nameTable.Columns.Add("PrimaryName", typeof(string));
+            nameTable.Columns.Add("BirthYear", typeof(int));
+            nameTable.Columns.Add("DeathYear", typeof(int));
+
+            foreach (Name_Model name in names)
+            {
+                nameTable.Rows.Add(name.NConst, name.PrimaryName, name.BirthYear ?? (object)DBNull.Value, name.DeathYear ?? (object)DBNull.Value);
+            }
+            SqlBulkCopy bulkCopy = new SqlBulkCopy(sqlConn)
+            {
+                DestinationTableName = "Names"
+            };
+
+            bulkCopy.WriteToServer(nameTable);
+        }
+    }
+}
