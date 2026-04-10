50f16729c9fe2dcce9cd65f617bf2537fa28697d
Martin Frost Lauridsen <mal021@edu.zealand.dk>
2026-04-10 10:02:19 +0200

    igen

--- Program.cs
+++ Program.cs
@@ -367,33 +367,6 @@ class Program
 //BulkNamePrimaryProfessionDataInsert.NameProfessionInserting();
 //BulkTitleCrewDataInsert.TitleCrewInserting();
 
-//Stopwatch stopwatch = Stopwatch.StartNew();
-
-////foreach (Title_Model movie in movies)
-////{
-////    Console.WriteLine(movie);
-////}
-
-//stopwatch.Stop();
-//Console.WriteLine("Elapsed milliseconds to read from file: " + stopwatch.ElapsedMilliseconds);
-
-//stopwatch.Start();
-
-////IInserter inserter = new NormalInserter();
-////IInserter inserter = new PreparedInserter();
-//IInserter inserter = new BulkInserter();
-
-//SqlConnection sqlConn = new SqlConnection(
-//    "Server=localhost;Database=MovieDB;Integrated security=True;" +
-//    "Trusted_Connection=True;TrustServerCertificate=True;");
-//sqlConn.Open();
-//inserter.InsertTitles(movies, sqlConn);
-
-//sqlConn.Close();
-//stopwatch.Stop(); // 3800 milconds for 10000 records, 4.4 seconds
-//// 2800 for prepared
-//Console.WriteLine("Elapsed milliseconds to Insert Data: " + stopwatch.ElapsedMilliseconds);
-
 
 
 
